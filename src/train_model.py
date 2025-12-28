b6ba11ee3c81d621db56f182351a10da757dbdb7
unknown <chris.qiu52@gmail.com>
2025-12-27 19:38:48 -0500

    model had 57% acc

--- src/train_model.py
+++ src/train_model.py
@@ -1,20 +1,16 @@
 import pandas as pd
 from sklearn.model_selection import train_test_split
-from sklearn.linear_model import LogisticRegression
-from sklearn.metrics import accuracy_score, classification_report
-from xgboost import XGBClassifier
+from sklearn.preprocessing import StandardScaler
+import xgboost as xgb
+import joblib
 
-df = pd.read_csv("merged_dataset.csv")
-
-df["wins_diff"] = df["t1_wins"] - df["t2_wins"]
-df["map_diff_diff"] = df["t1_map_diff"] - df["t2_map_diff"]
-df["round_diff_diff"] = df["t1_round_diff"] - df["t2_round_diff"]
+df = pd.read_csv("../data/matches_dataset.csv")
 
 # features we want the model to to train on
 FEATURES = [
-    "wins_diff",
-    "map_diff_diff",
-    "round_diff_diff",
+    'winrate_diff',
+    'form_diff',
+    'rnd_diff_diff'
 ]
 
 # inputting the feature
@@ -28,28 +24,19 @@ X_train, X_test, y_train, y_test = train_test_split(
     test_size=0.2,
     # randomizes the split, but split is same if you use the same seed (42)
     random_state=42,
-    # ensures test matches reality, prevents misleading accuracy
     stratify=y
 )
 
-# xgboost
-model = XGBClassifier(
+# xgboost train
+model = xgb.XGBClassifier(
     # num of trees, more trees -> more learning capacity
     # too few -> underfit, too many -> overfit
-    n_estimators=200,
+    n_estimators=100,
     # depth of each tree, "if else splits"
     # shallow -> generalize better, deep -> memorizes patterns
-    max_depth=4,
+    max_depth=3,
     # essentially step sizes
-    learning_rate=0.05,
-    # fraction of matches used to train each tree (each only see 80% of matches, randomly sampled)
-    # reduce overfitting by not letting it "memorize everything"
-    subsample=0.8,
-    # fraction of features used by each tree, prevent one feature from dominating
-    # so not all the features of shown for all trees
-    colsample_bytree=0.8,
-    # defines what the task is, and the output is a probability
-    objective="binary:logistic",
+    learning_rate=0.1,
     # evals how wrong it is, penalizes confident wrong prediction, encourages calibrated probabilities
     eval_metric="logloss",
     random_state=42
@@ -58,13 +45,11 @@ model = XGBClassifier(
 #train the model
 model.fit(X_train, y_train)
 
-# predict probabilities
-y_proba = model.predict_proba(X_test)[:, 1]
+# save
+# this contains the decision trees with their split and rules
+joblib.dump(model, '../model/model.pkl')
 
-# convert probabilities to class labels
-y_pred = (y_proba >= 0.5).astype(int)
-
-print("Accuracy:", accuracy_score(y_test, y_pred))
-print(classification_report(y_test, y_pred))
-
-
+# Quick test
+accuracy = model.score(X_test, y_test)
+print(f"Model accuracy: {accuracy:.1%}")
+print("Model saved as model.pkl")
