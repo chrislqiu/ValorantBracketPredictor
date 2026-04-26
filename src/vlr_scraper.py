ed8b3b9e77337f8f447e83c09d6b303117ec82ab
Chris Qiu <chris.qiu52@gmail.com>
2026-04-25 22:16:58 -0400

    changed to scrape from page 26 (start of this year)

--- src/vlr_scraper.py
+++ src/vlr_scraper.py
@@ -144,6 +144,6 @@ def write_to_json(data, filename = "../data/matches_raw.json"):
 
 if __name__ == "__main__":
 
-    matches = scrape_multiple_pages(230)
+    matches = scrape_multiple_pages(26)
     write_to_json(matches)
     print(f"Wrote to file")
